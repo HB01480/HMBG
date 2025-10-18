1b5f35734151b31da28c434ae73a4931c4613d47
HB01480 <nazmulphy@gmail.com>
2025-10-18 21:10:56 +0600

    Added application layer base class

--- src/application.hh
+++ src/application.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include "common.hh"
+#include "applicationLayer.hh"
 
 
 class Application {
---
+++ src/applicationLayer.hh
@@ -0,0 +1,19 @@
+#pragma once
+#include "common.hh"
+
+
+class ApplicationLayer {
+public:
+    ~ApplicationLayer() = default;
+
+    virtual void onEnter() = 0;
+    virtual void onExit() = 0;
+
+    virtual void update(f32 dt) = 0;
+    virtual void render(SDL_Renderer *renderer) = 0;
+    virtual void processEvent(SDL_Event *event) = 0;
+
+private:
+
+
+};
