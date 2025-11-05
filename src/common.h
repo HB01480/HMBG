a075aac6af15bb81ffed0176c664119e38edf0db
HB01480 <nazmulphy@gmail.com>
2025-11-05 16:45:31 +0600

    Added typedefs and bool definitions

--- src/common.h
+++ src/common.h
@@ -4,3 +4,28 @@
 #include <SDL3_ttf/SDL_ttf.h>
 
 
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef signed char s8;
+typedef short s16;
+typedef int s32;
+typedef long long s64;
+
+typedef float f32;
+typedef double f64;
+
+
+#ifndef bool
+#define bool _Bool
+#endif
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef false
+#define false 0
+#endif
