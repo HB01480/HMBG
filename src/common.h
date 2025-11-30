056abdbe6726d9b76f4e44e8d458119c495c8593
HB01480 <nazmulphy@gmail.com>
2025-11-30 17:21:26 +0600

    Now using CGLM's struct API now

--- src/common.h
+++ src/common.h
@@ -2,7 +2,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
-#include <cglm/cglm.h>
+#include <cglm/struct.h>
 
 #include "utils/typedefs.h"
 #include "utils/bool.h"
