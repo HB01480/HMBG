713111e1a00e5920ed37438309a41c78d1b0022e
HB01480 <nazmulphy@gmail.com>
2025-11-30 17:31:24 +0600

    Updated basic shaders AGAIN (Fragment shader specifically)

--- src/shaders/basic.frag.glsl
+++ src/shaders/basic.frag.glsl
@@ -12,6 +12,10 @@ layout (std140, set = 3, binding = 0) uniform BasicUBO {
 
 
 void main() {
-    float pulse = sin(time) * 0.5f + 0.5f;
-    outColor = vec4(0.875f, 0.25f, 0.25f, 1.0f) * pulse;
+    float pulse1 = sin(time) * 0.5f + 0.5f;
+    float pulse2 = cos(time) * 0.5f + 0.5f;
+    float pulse3 = pulse1 * pulse2;
+    vec3 baseColor = vec3(0.875f, 0.25f, 0.25f);
+
+    outColor = vec4(baseColor.r * pulse1, baseColor.g * pulse2, baseColor.b * pulse3, 1.0f);
 }
