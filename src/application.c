b968645bd0707be6b865acb2c4e591c624fc9ca8
HB01480 <nazmulphy@gmail.com>
2025-12-01 17:02:41 +0600

    Added deltatime to model matrix rotation

--- src/application.c
+++ src/application.c
@@ -271,7 +271,7 @@ SDL_AppResult application_onUpdate(Application *app) {
     if (app->keyState[SDL_SCANCODE_A])
         renderCamera_move(&app->camera, CM_LEFTWARD, app->clock.dt);
 
-    app->basicUBO.model = glms_rotate(app->basicUBO.model, 45.0f * DEGREES_TO_RADIANS, (vec3s){{0.0f, 0.0f, 1.0f}});
+    app->basicUBO.model = glms_rotate(app->basicUBO.model, 45.0f * DEGREES_TO_RADIANS * app->clock.dt, (vec3s){{0.0f, 0.0f, 1.0f}});
     app->basicUBO.view = renderCamera_calculateViewMatrix(&app->camera);
     app->basicUBO.projection = application_calculatePerspectiveMatrixFromWindow(app->window);
     app->basicUBO.time = SDL_GetTicks() / 1000.0f;
