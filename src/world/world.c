ed49aaff60f4752c4a228df65a60c5a74f4a0f5f
HB01480 <nazmulphy@gmail.com>
2025-12-17 15:56:24 +0600

    somewhat implemented world's functionality for mainly testing purposes

--- src/world/world.c
+++ src/world/world.c
@@ -1,3 +1,27 @@
 #include "world.h"
 
 
+World world_init(u64 seed) {
+    World world; SDL_zero(world);
+    world.seed = seed;
+
+    return world;
+}
+
+void world_free(World *world) {
+    // Nothing for now
+}
+
+Chunk *world_generateTestChunk(World *world) {
+    Chunk *chunk = chunk_init();
+
+    for (u32 x = 0; x < CHUNK_SIZE_X; x++) {
+        for (u32 y = 0; y < CHUNK_SIZE_Y; y++) {
+            for (u32 z = 0; z < CHUNK_SIZE_Z; z++) {
+                chunk_setBlock(chunk, x, y, z, BLOCK_ID_STONE);
+            }
+        }
+    }
+
+    return chunk;
+}
--- src/world/world.h
+++ src/world/world.h
@@ -1,9 +1,16 @@
 #pragma once
-#include "worldConstants.h"
 #include "../common.h"
+#include "worldConstants.h"
+#include "chunk.h"
 
 
 typedef struct World {
     u64 seed;
 
 } World;
+
+
+World world_init(u64 seed);
+void world_free(World *world);
+
+Chunk *world_generateTestChunk(World *world);
