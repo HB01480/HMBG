c8b4cb421727fdd25f7f96fd4624872aa57118d6
HB01480 <nazmulphy@gmail.com>
2025-12-23 14:59:24 +0600

    Modified the shader compilation script to compile the textured shaders

--- compileShaders.ps1
+++ compileShaders.ps1
@@ -9,3 +9,6 @@ glslc -fshader-stage=fragment $srcDirectory/primitive.frag.glsl -o $outDirectory
 
 glslc -fshader-stage=vertex $srcDirectory/basic.vert.glsl -o $outDirectory/basic.vert.spv
 glslc -fshader-stage=fragment $srcDirectory/basic.frag.glsl -o $outDirectory/basic.frag.spv
+
+glslc -fshader-stage=vertex $srcDirectory/textured.vert.glsl -o $outDirectory/textured.vert.spv
+glslc -fshader-stage=fragment $srcDirectory/textured.frag.glsl -o $outDirectory/textured.frag.spv
