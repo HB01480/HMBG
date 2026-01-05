12835d0990a9c80b24b348376da1431c16a7e284
HB01480 <nazmulphy@gmail.com>
2026-01-06 00:25:15 +0600

    Fixed a bug in the textured fragment shader

--- src/shaders/textured.frag.glsl
+++ src/shaders/textured.frag.glsl
@@ -4,7 +4,7 @@ layout (location = 0) in vec2 fTexCoord;
 
 layout (location = 0) out vec4 outColor;
 
-layout (set = 2, binding = 1) uniform sampler2D diffuseSampler;
+layout (set = 2, binding = 0) uniform sampler2D diffuseSampler;
 
 
 
