4cd62f178b33b7b541ec861f9845685f96352e5f
HB01480 <nazmulphy@gmail.com>
2026-01-05 13:21:05 +0600

    renamed 'APP_ERROR_RETURN_SDL' macro to 'HANDLE_SDL_ERROR'

--- src/utils/error_macros.h
+++ src/utils/error_macros.h
@@ -1,7 +1,7 @@
 #pragma once
 
 
-#define APP_ERROR_RETURN_SDL(errorCondition, errorText, handleErrorLabel)\
+#define HANDLE_SDL_ERROR(errorCondition, errorText, handleErrorLabel)\
 if ((errorCondition)){\
     SDL_Log("%s: %s", errorText, SDL_GetError());\
     goto handleErrorLabel;\
