76ebbfe3b77ea0ce6b759a13c6b06483330f893b
HB01480 <nazmulphy@gmail.com>
2026-01-06 00:55:43 +0600

    Significantly improved the error macros

--- src/utils/error_macros.h
+++ src/utils/error_macros.h
@@ -3,12 +3,12 @@
 
 #define HANDLE_SDL_ERROR(errorCondition, errorText, handleErrorLabel)\
 if ((errorCondition)){\
-    SDL_Log("%s: %s", errorText, SDL_GetError());\
+    SDL_Log("%s: %s: %s", __func__, errorText, SDL_GetError());\
     goto handleErrorLabel;\
 }
 
 #define HANDLE_ERROR(errorCondition, errorText, handleErrorLabel)\
 if ((errorCondition)){\
-    SDL_Log("%s", errorText);\
+    SDL_Log("%s: %s", __func__, errorText);\
     goto handleErrorLabel;\
 }
