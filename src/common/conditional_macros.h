01cd230ebe06c248ff563ef0ad5915bb683be591
HB01480 <nazmulphy@gmail.com>
2026-02-03 17:26:13 +0600

    Made the conditional macros use the new format string version of the print macros

--- src/common/conditional_macros.h
+++ src/common/conditional_macros.h
@@ -4,12 +4,12 @@
 #include "print_macros.h"
 
 
-#define HANDLE_CRITICAL(condition, handleLabel, text) if ((condition)) { PRINT_CRITICAL(text) goto handleLabel; }
-#define HANDLE_ERROR(condition, handleLabel, text) if ((condition)) { PRINT_ERROR(text) goto handleLabel; }
-#define CHECK_ERROR(condition, text) if ((condition)) { PRINT_ERROR(text) }
-#define CHECK_WARNING(condition, text) if ((condition)) { PRINT_WARNING(text) }
+#define HANDLE_CRITICAL(condition, handleLabel, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_CRITICAL(formatLiteral, __VA_ARGS__) goto handleLabel; }
+#define HANDLE_ERROR(condition, handleLabel, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_ERROR(formatLiteral, __VA_ARGS__) goto handleLabel; }
+#define CHECK_ERROR(condition, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_ERROR(formatLiteral, __VA_ARGS__) }
+#define CHECK_WARNING(condition, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_WARNING(formatLiteral, __VA_ARGS__) }
 
-#define HANDLE_SDL_CRITICAL(condition, handleLabel, text) if ((condition)) { PRINT_SDL_CRITICAL(text) goto handleLabel; }
-#define HANDLE_SDL_ERROR(condition, handleLabel, text) if ((condition)) { PRINT_SDL_ERROR(text) goto handleLabel; }
-#define CHECK_SDL_ERROR(condition, text) if ((condition)) { PRINT_SDL_ERROR(text) }
-#define CHECK_SDL_WARNING(condition, text) if ((condition)) { PRINT_SDL_WARNING(text) }
+#define HANDLE_SDL_CRITICAL(condition, handleLabel, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_SDL_CRITICAL(formatLiteral, __VA_ARGS__) goto handleLabel; }
+#define HANDLE_SDL_ERROR(condition, handleLabel, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_SDL_ERROR(formatLiteral, __VA_ARGS__) goto handleLabel; }
+#define CHECK_SDL_ERROR(condition, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_SDL_ERROR(formatLiteral, __VA_ARGS__) }
+#define CHECK_SDL_WARNING(condition, formatLiteral, ...) if ((condition)) { PRINT_FORMAT_SDL_WARNING(formatLiteral, __VA_ARGS__) }
